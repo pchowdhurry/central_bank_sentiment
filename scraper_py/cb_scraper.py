2569fe254e37a5f8b4950d9792d469834021fc71
Prottoya Chowdhury <prottoya_chowdhury@brown.edu>
2025-05-29 17:34:34 -0400

    fed speech scraper class written

--- scraper_py/cb_scraper.py
+++
@@ -1,8 +0,0 @@
-import os 
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-
-BASE_URL = "https://www.federalreserve.gov"
-SPEECH_URL = 'https://www.federalreserve.gov/newsevents/speeches.htm'
-
